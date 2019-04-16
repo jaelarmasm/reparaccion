15ef76e9de318c3731aa7732e2c1a96cc9dcdc70
Byron Rosas <byron_mma@hotmail.es>
2019-04-16 11:22:51 -0500

    Se creo los archivos de los controladores para cada uno de los modelos

---
+++ app/Http/Controllers/ContratistaController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Contratista;
+use Illuminate\Http\Request;
+
+class ContratistaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Contratista  $contratista
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contratista $contratista)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Contratista  $contratista
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contratista $contratista)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Contratista  $contratista
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contratista $contratista)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TipoTrabajoController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\TipoTrabajo;
+use Illuminate\Http\Request;
+
+class TipoTrabajoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\TipoTrabajo  $tipoTrabajo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TipoTrabajo $tipoTrabajo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\TipoTrabajo  $tipoTrabajo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TipoTrabajo $tipoTrabajo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\TipoTrabajo  $tipoTrabajo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TipoTrabajo $tipoTrabajo)
+    {
+        //
+    }
+}
