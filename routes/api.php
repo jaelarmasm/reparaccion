943e46fba84a7cacd0fb99392c62a8681634ac77
Byron Rosas <byron_mma@hotmail.es>
2019-04-26 10:51:39 -0500

    se añadio cors exception

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(['middleware' => 'cors', 'prefix' => 'api'], function()
+{
+    Route::resource('anuncio', 'AnuncioController');
+    Route::resource('contratista', 'ContratistaController');
+    Route::resource('contrato', 'ContratoController');
+    Route::resource('estado', 'EstadoController');
+    Route::resource('plan', 'PlanController');
+    Route::resource('tipotrabajo', 'TipoTrabajoController');
 
-Route::resource('anuncio', 'AnuncioController');
-Route::resource('contratista', 'ContratistaController');
-Route::resource('contrato', 'ContratoController');
-Route::resource('estado', 'EstadoController');
-Route::resource('plan', 'PlanController');
-Route::resource('tipotrabajo', 'TipoTrabajoController');
+}
